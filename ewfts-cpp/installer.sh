2bf7bf9ce9a75a527b33e8812d470632de14f16e
kban3108-max <kban3108@gmail.com>
2026-07-16 17:20:02 +0300

    more C++ slop updates... i have enough C++ slop to feed you for a lifetime

---
+++ ewfts-cpp/installer.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+printf "Install EWFTS? [y/N]: "
+read answer
+
+if [ "$answer" = "y" ]; then
+    make || exit 1
+    mkdir -p "$HOME/.local/bin"
+    mkdir -p "$HOME/.local/share/man/man1"
+    cp ewfts "$HOME/.local/bin/ewfts"
+    [ -f ewfts.1 ] && cp ewfts.1 "$HOME/.local/share/man/man1/ewfts.1"
+    echo "Installed."
+    echo "Make sure ~/.local/bin is in path (and make sure ~/.local/share/man is in MANPATH to use the manpage)"
+else
+    echo "Cancelled."
+fi
